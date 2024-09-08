79a928198de2306a83ed0d54d4ab21c20ab70618
abhishek <aksingh1997@gmail.com>
2024-09-08 21:28:05 +0530

    First commit

---
+++ src/test/java/com/abhi/JavaThreads/JavaThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.JavaThreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
