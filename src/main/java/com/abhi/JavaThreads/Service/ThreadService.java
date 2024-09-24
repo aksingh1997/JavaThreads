8cae187781bd99667ce75007ee2d7f8c070038c8
abhishek <aksingh1997@gmail.com>
2024-09-24 08:12:13 +0530

    Explored different locks

--- src/main/java/com/abhi/JavaThreads/Service/ThreadService.java
+++ src/main/java/com/abhi/JavaThreads/Service/ThreadService.java
@@ -5,16 +5,29 @@ import com.abhi.JavaThreads.models.Thread1;
 import com.abhi.JavaThreads.models.Thread2;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.BiFunction;
+
 @Service
 public class ThreadService {
+
+    Data data;
 
     Thread t1 = null;
     Thread t2 = null;
 
-    public ThreadService() {
+    public ThreadService(Data data) {
+        this.data = data;
         //executeThreadsByExtendingThreadClass();
         //executeThreadsByImplementingRunnable();
-        executeSynchronizedThreads();
+        //executeSynchronizedThreads();
+        //reentrantLock();
+        //waitNotifyThreads();
+        //conditionalLock();
+        readWriteLock();
+
     }
 
     public void executeThreadsByExtendingThreadClass() {
@@ -40,5 +53,87 @@ public class ThreadService {
         s2.start();
     }
 
-    public void Lo
+    // we can use wait() and notify() for threads
+    // producer will only produce when consumer notifies. Otherwise producer thread will wait.
+    // Note: Even when thread is in waiting, no other thread is allowed to enter in synchronised block
+    public void waitNotifyThreads() {
+
+        for(int i = 0; i < 10; i++) {
+            if(i % 2 == 0)
+                new Thread(() -> data.produce(), String.valueOf(i)).start();
+            else
+                new Thread(() -> data.consume(), String.valueOf(i)).start();
+        }
+    }
+
+    public void conditionalLock() {
+        for(int i = 0; i < 100; i++) {
+            new Thread(() -> data.consumeBiscuits()).start();
+        }
+        for(int i = 0; i < 100; i++) {
+            new Thread(() -> data.produceBiscuits()).start();
+        }
+
+    }
+    // Reentrant locks are used in case of recursive calls and inner method calls. A thread holding a lock of Reentrant lock can acquire it again.
+    // Once a thread acquires a Reentrant lock, it can acquire it again, but other threads cannot acquire the same lock.
+    // we can take example of gcd()
+    public void reentrantLock() {
+        BiFunction<Integer, Integer, Integer> printGcd = (x, y) -> data.gcd(x, y);
+        for(int i = 1; i < 10; i++) {
+            for(int j = 1; j < 20; j++) {
+                // we need to create final variables here as lambda exp wont accept non final variables.
+                // Reason = Thread creation line contains gcd(x, y). Here x and y will be replaced by its exact value in lambda at compile time. So it should be constant.
+                final int x = i;
+                final int y = j;
+                new Thread(() -> System.out.println("gcd of " + x + " and " + y + " = " + data.gcd(x, y) + "\n\n")).start();
+                // same thread will put lock on gcd method in recursion, increases its counter on each call, and decreases the lock counter while returning.
+
+                // Expected output is of type -
+                /*
+                    Thread-151 ==> 1
+                    Thread-151 ==> 2
+                    Thread-151 ==> 3
+                    gcd of 8 and 15 = 1
+                */
+
+                // But sometimes we can also get output like -
+                /*
+                    Thread-155 ==> 1
+                    Thread-155 ==> 2
+                    Thread-155 ==> 3
+                    Thread-155 ==> 4
+                    Thread-157 ==> 1
+                    Thread-157 ==> 2
+                    gcd of 9 and 2 = 1
+                 */
+
+                // this is due to reason that lock is released by Thread 155 and the time when result comes back and prints it in ThreadService class, Thread-157 has
+                // acquired lock already.
+            }
+        }
+    }
+
+    public void readWriteLock() {
+        Random rand = new Random();
+        for(int i = 0; i < 100; i++) {
+            int p = rand.nextInt(3);
+            Thread thread = null;
+            switch (p) {
+                case 0:
+                    thread = new Thread(() -> data.withdrawAmount(300));
+                    break;
+                case 1:
+                    thread = new Thread(() -> data.depositAmount(100));
+                    break;
+                case 2:
+                    thread = new Thread(() -> data.readBalance());
+                    break;
+                default:
+                    System.out.println("Wrong choice");
+                    break;
+            }
+            thread.start();
+        }
+    }
 }
--- src/main/java/com/abhi/JavaThreads/models/Data.java
+++ src/main/java/com/abhi/JavaThreads/models/Data.java
@@ -1,10 +1,31 @@
 package com.abhi.JavaThreads.models;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
+import java.util.concurrent.locks.*;
 
+@Component
 public class Data {
-    public static ArrayList<Integer> ls;
 
+    ReentrantLock gcdLock = new ReentrantLock();
+    //ReentrantLock gcdLock = new ReentrantLock(true); // provide true as input as fairness flag, default is false. Fairness means FIFO/FCFS. This may reduce speed.
+
+    // biscuit producer and consumer problem related variables
+    int noOfBiscuits = 0;
+    int inventorySize = 60;
+    Lock biscuitLock = new ReentrantLock();
+    Condition notFull = biscuitLock.newCondition();
+    Condition notEmpty = biscuitLock.newCondition();
+
+    // banking read write lock variables
+    int accountBalance = 1000;
+    ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    Lock readLock = readWriteLock.readLock();
+    Lock writeLock = readWriteLock.writeLock();
+
+
+    public static ArrayList<Integer> ls;
     static {
         ls = new ArrayList<>();
         for(int i = 0; i < 10; i++)
@@ -32,10 +53,141 @@ public class Data {
                     Thread.sleep(500);
                     System.out.println(Thread.currentThread().getName() + " " + l);
                 }
-            } catch(InterruptedException ex) {
+            } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
         }
+    }
 
+    //Notice that consumer and producer have same lock applied -> object of this class.
+    public synchronized void produce() {
+        try {
+            wait(); // wait until consumer has consumed and notifies the object lock.
+            System.out.println( Thread.currentThread().getName() + " producing-------");
+            Thread.sleep(1);
+            System.out.println(Thread.currentThread().getName() + " produced-------");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    public synchronized void consume() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " consuming--------");
+            Thread.sleep(1);
+            System.out.println(Thread.currentThread().getName() + " consumed--------");
+            notify(); // notify the waiting producers to start producing
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void produceBiscuits() {
+        biscuitLock.lock();
+        try {
+            while(noOfBiscuits == inventorySize) { //Here we are using while loop instead of if , so that whenever a thread wakes up , it should check this condition again and proceed
+                notFull.await(); // using await in a condition causes thread to release the lock temporarily until is it awakened by signal or interruption
+            }
+            Thread.sleep(10); // using sleep does not cause Thread to release the lock. Thread holds the lock during sleep.
+            noOfBiscuits++;
+            System.out.println("Biscuit produced. Total biscuit = " + noOfBiscuits);
+            notEmpty.signal(); // since the biscuit is produced so inventory is not empty now, signal the consumer to consume
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        biscuitLock.unlock();
+
+    }
+    public void consumeBiscuits() {
+        biscuitLock.lock();
+        try {
+            while(noOfBiscuits == 0) {
+                notEmpty.await();
+            }
+            Thread.sleep(10);
+            noOfBiscuits--;
+            System.out.println("Biscuit consumed. Total biscuit = " + noOfBiscuits);
+            notFull.signal(); // since the biscuit is consumed so inventory in not full now, signal the producer to produce more
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        biscuitLock.unlock();
+    }
+
+    public void reentrantLock() {
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        try {
+            for (Integer l : ls) {
+                Thread.sleep(500);
+                System.out.println(Thread.currentThread().getName() + " " + l);
+                
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int gcd(int x, int y) {
+
+        if(y == 0)
+            return x;
+
+        // swap if x < y
+        gcdLock.lock();
+        System.out.println(Thread.currentThread().getName() + " ==> " + gcdLock.getHoldCount());
+        if(y > x) {
+            x += y;
+            y = x - y;
+            x = x - y;
+        }
+        int p = gcd(y, x % y);
+        gcdLock.unlock();
+        return p;
+    }
+
+    // withdraw amount by putting write lock
+    public void withdrawAmount(int money) {
+        writeLock.lock();
+        try {
+            if(accountBalance - money < 0) {
+                System.out.println("Insufficient balance");
+                return;
+            }
+            Thread.sleep(200);
+            accountBalance -= money;
+            System.out.println("Withdraw:: " + money + ", Total Balance:: " + accountBalance);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        writeLock.unlock();
+    }
+
+    // deposit amount by putting write lock
+    public void depositAmount(int money) {
+        writeLock.lock();
+        try {
+            Thread.sleep(350);
+            accountBalance += money;
+            System.out.println("Deposit:: " + money + ", Total Balance:: " + accountBalance);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        writeLock.unlock();
+    }
+
+    // view account balance by putting read lock
+    public void readBalance() {
+        readLock.lock();
+        try {
+            Thread.sleep(200);
+            System.out.println("Total Balance:: " + accountBalance);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        readLock.unlock();
     }
 }
